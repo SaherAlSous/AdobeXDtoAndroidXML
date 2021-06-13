91b2c8ac6a662ee86eef8a32f5e959ec089d251b
Saher AlSous <saher.alsous@gmail.com>
2021-06-13 20:00:46 +0300

    Final-work Submit

--- app/src/main/java/com/bignerdranch/android/adobexdtoandroidxml/SecondActivity.kt
+++ app/src/main/java/com/bignerdranch/android/adobexdtoandroidxml/SecondActivity.kt
@@ -1,8 +1,11 @@
 package com.bignerdranch.android.adobexdtoandroidxml
 
+import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Pair
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
@@ -64,7 +67,11 @@ class SecondActivity : AppCompatActivity() {
 
         up_arrow.setOnClickListener {
             val intent = Intent(this, ThirdActivity::class.java)
-            startActivity(intent)
+
+            val pairs = Pair<View, String>(up_arrow,"background_image_transition")
+            val activityOption = ActivityOptions.makeSceneTransitionAnimation(this, pairs)
+
+            startActivity(intent, activityOption.toBundle())
         }
 
         second_back_arrow.setOnClickListener {
--- app/src/main/java/com/bignerdranch/android/adobexdtoandroidxml/ThirdActivity.kt
+++ app/src/main/java/com/bignerdranch/android/adobexdtoandroidxml/ThirdActivity.kt
@@ -2,10 +2,32 @@ package com.bignerdranch.android.adobexdtoandroidxml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.ScrollView
 
 class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var down_arrow : ImageView
+    private lateinit var scroll_view : ScrollView
+    private lateinit var animation : Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+        down_arrow = findViewById(R.id.down_arrow)
+        scroll_view = findViewById(R.id.scroll_view)
+        animation = AnimationUtils.loadAnimation(this, R.anim.anim_from_bottom)
+        scroll_view.animation = animation
+
+
+        down_arrow.setOnClickListener {
+            finish()
+        }
+
+
+
+
     }
 }
